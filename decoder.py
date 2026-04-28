db62a89ac06abfb3271df17b4914ca7dc1196956
bshane482 <234391001+bshane482@users.noreply.github.com>
2026-04-27 18:18:09 -0700

    init structure

---
+++ decoder.py
@@ -0,0 +1,12 @@
+import wave
+import numpy as np
+
+
+FS = 48000
+BIT_RATE = 300
+SAMPLES_PER_BIT = 160
+FREQ_SPACE = 2025
+FREQ_MARK = 2225
+
+if __name__ == "__main__":
+    print("This is the modem demodulator module.")
